60c8a61a4b02a1075df81ed78a121c9984207e8f
N F Heathward <leafield@hotmail.com>
2015-11-01 17:21:31 +0000

    First commit

---
+++ D1_Exercise_1.1.B.groovy
@@ -0,0 +1,5 @@
+print "Please key in a word: "
+String str1 = System.console().readLine()
+print "And now key in another: "
+String str2 = System.console().readLine()
+println "You have typed: " + str1 + " " + str2 + "."
