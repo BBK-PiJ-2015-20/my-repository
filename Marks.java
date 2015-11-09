9d89ce51698ea0d09af55d9056740e63467d2474
Nicole Heathward <leafield@hotmail.com>
2015-11-09 20:45:58 +0000

    First commit

---
+++ Marks.java
@@ -0,0 +1,13 @@
+public class Marks {
+	public static int distinctions;
+	public static int passes;
+	public static int fails;
+	public static int invalids;
+	
+	public static void main(String[] args) {
+		}
+	}
+	
+	public void readList(String mark) {
+		}
+	
